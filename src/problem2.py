17ccf3245329acf5d2b6dea027035fc13e8cb517
bowmanj1 <bowmanj1@rose-hulman.edu>
2018-10-25 21:41:59 -0400

    j

--- src/problem2.py
+++ src/problem2.py
@@ -2,9 +2,9 @@
 Exam 3, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Jacob Bowman.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -90,10 +90,14 @@ def problem2(sequence):
       :type sequence [list]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    a = 0
+    for k in range(len(sequence)):
+        if abs(sequence[k]) > abs(sequence[a]):
+            a = k
+    return a
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
